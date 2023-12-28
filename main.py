86350b821e864efcd74bd88358f3c4e36b7f760c
rangel rocha <rangelanrn@hotmail.com>
2023-12-27 21:31:41 -0300

    Realizadas alterações no adminpanel para a adição de usuários

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from ui.login import LoginPanel
+from ui.adminpanel import AdminPanel
+from data.admin import Admin
 import tkinter as tk
 
 if __name__ == "__main__":
     root_login = tk.Tk()
-    tela_login = LoginPanel(root_login)
+    tela_login = AdminPanel(root_login,Admin("rangel"))
     root_login.mainloop()
--- ui/adminpanel.py
+++ ui/adminpanel.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
+
 
 class AdminPanel:
     """
@@ -21,21 +22,24 @@ class AdminPanel:
         Parâmetros:
             root (Tk): A janela principal da aplicação.
         """
-        self.root = root
-        self.root.title("Admin Panel")
-        self.root.geometry("800x600")
+        #Definindo variaveis de usuário
+        self.__user = user
 
-        self.label_title = tk.Label(root, text="Lista de Usuários", font=("Helvetica", 24))
-        self.label_title.pack(pady=20)
+        self.__root = root
+        self.__root.title("Admin Panel")
+        self.__root.geometry("800x600")
 
-        # Exemplo de uma lista de usuários (pode ser substituída por uma estrutura de dados real)
-        self.lista_users = ["User 1", "User 2", "User 3"]
+        self.__label_title = tk.Label(root, text="Lista de Usuários", font=("Helvetica", 24))
+        self.__label_title.pack(pady=20)
+
+        # TODO Exemplo de uma lista de usuários (pode ser substituída por uma estrutura de dados real)
+        self.__lista_users = ["User 1", "User 2", "User 3"]
 
         # Exibindo a lista de usuários
-        self.listbox_users = tk.Listbox(root)
-        for user in self.lista_users:
-            self.listbox_users.insert(tk.END, user)
-        self.listbox_users.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)  # Ocupa toda a largura com margens de 20px
+        self.__listbox_users = tk.Listbox(root)
+        for user in self.__lista_users:
+            self.__listbox_users.insert(tk.END, user)
+        self.__listbox_users.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)  # Ocupa toda a largura com margens de 20px
 
         # Botões de ação
         self.button_add = tk.Button(root, text="Adicionar Usuário", command=self.add_user)
@@ -51,22 +55,26 @@ class AdminPanel:
         """
         Adiciona um novo usuário à lista.
         """
-        new_user = "Novo Usuário"
-        self.lista_users.append(new_user)
-        self.listbox_users.insert(tk.END, new_user)
-        messagebox.showinfo("Admin Panel", f"Usuário '{new_user}' adicionado com sucesso!")
+        username=simpledialog.askstring("Adicionar usuario","Digite o nome do usuário: ",parent=self.__root)
+        password=simpledialog.askstring("Adicionar usuario","Digite a senha do usuário: ",parent=self.__root)
+        user_type=simpledialog.askstring("Adicionar usuario","Qual o cargo do usuário (Admin, Cashier, Stockist): ",parent=self.__root)
+        if self.__user.add_user(username,password,user_type):
+            self.__listbox_users.insert(tk.END, username)
+        else:
+            messagebox.showerror("Adicionar usuario","Usuário já existe no sistema.")
+        
 
     def edit_user(self):
         """
         Edita o usuário selecionado na lista.
         """
-        selected_index = self.listbox_users.curselection()
+        selected_index = self.__listbox_users.curselection()
         if selected_index:
-            selected_user = self.lista_users[selected_index[0]]
+            selected_user = self.__lista_users[selected_index[0]]
             new_name = "Novo Nome"  # Substitua por uma caixa de diálogo para obter um novo nome
-            self.lista_users[selected_index[0]] = new_name
-            self.listbox_users.delete(selected_index)
-            self.listbox_users.insert(selected_index, new_name)
+            self.__lista_users[selected_index[0]] = new_name
+            self.__listbox_users.delete(selected_index)
+            self.__listbox_users.insert(selected_index, new_name)
             messagebox.showinfo("Admin Panel", f"Usuário '{selected_user}' editado para '{new_name}' com sucesso!")
         else:
             messagebox.showwarning("Admin Panel", "Selecione um usuário para editar.")
@@ -75,11 +83,19 @@ class AdminPanel:
         """
         Desativa o usuário selecionado na lista.
         """
-        selected_index = self.listbox_users.curselection()
+        selected_index = self.__listbox_users.curselection()
         if selected_index:
-            selected_user = self.lista_users[selected_index[0]]
-            del self.lista_users[selected_index[0]]
-            self.listbox_users.delete(selected_index)
+            selected_user = self.__lista_users[selected_index[0]]
+            del self.__lista_users[selected_index[0]]
+            self.__listbox_users.delete(selected_index)
             messagebox.showinfo("Admin Panel", f"Usuário '{selected_user}' desativado com sucesso!")
         else:
             messagebox.showwarning("Admin Panel", "Selecione um usuário para desativar.")
+
+        def active_user(self):
+            """
+            Ativa o usuário selecionado na lista.
+            """
+        selected_index = self.__listbox_users.curselection()
+        if selected_index:
+            selected_user = self.__lista_users[selected_index[0]]
